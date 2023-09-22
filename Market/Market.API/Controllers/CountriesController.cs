91d82f2a5477d455ac649bc5cc1014f36554b0c5
CESAR AUGUSTO BARRIENTOS CORDOBA <cesarbarrientos298871@correo.itm.edu.co>
2023-09-22 07:30:00 -0500

    create http methods for controller countries.

---
+++ Market/Market.API/Controllers/CountriesController.cs
@@ -0,0 +1,66 @@
+﻿using Market.API.Data;
+using Market.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPut] 
+        public async Task<ActionResult> Put(Country country)
+        {
+            _context.Update(country);   
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var afectedrow = await _context.Countries.Where(c => c.Id == id).ExecuteDeleteAsync();
+
+            if(afectedrow==0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
